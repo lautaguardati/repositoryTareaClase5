6c81f94f29b6cd3adea3ef3cba03def229f945cf
lautaguardati <lautaguardati@gmail.com>
2023-12-21 19:24:44 -0300

    Tarea 2
    Finalización de la tarea numero 2.
    Subida de archivo .html y .js.

---
+++ Tarea 2/tarea2.js
@@ -0,0 +1,34 @@
+const $botonIngresar = document.querySelector("#boton-ingreso");
+
+let footer = document.createElement("footer");
+footer.setAttribute ("footer", "myfooter");
+document.body.appendChild(footer);
+
+
+$botonIngresar.onclick = function () {
+    cambiarTextoTitulo();
+    mostrarInformacionUsuario();
+    return false
+}
+
+function cambiarTextoTitulo() {
+    const primerNombreUsuario = document.querySelector("#primer-nombre-usuario").value;
+
+    let textoTitulo = document.querySelector("h1");
+    textoTitulo.innerText = "Bienvenido " + primerNombreUsuario + "!";
+}
+
+function mostrarInformacionUsuario() {
+    const primerNombreUsuario = document.querySelector("#primer-nombre-usuario").value;
+    const segundoNombreUsuario = document.querySelector("#segundo-nombre-usuario").value;
+    const apellidoUsuario = document.querySelector("#apellidos-usuario").value;
+    const edadUsuario = Number(document.querySelector("#edad-usuario").value);
+
+    let nodoInformacion = document.createElement("p");
+    let informacionUsuario = document.createTextNode(`${primerNombreUsuario} ${segundoNombreUsuario} ${apellidoUsuario} edad ${edadUsuario}`);
+
+    nodoInformacion.appendChild(informacionUsuario);
+    footer.appendChild(nodoInformacion);
+}
+
+
